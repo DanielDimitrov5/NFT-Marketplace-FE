3ff9b5432d66084104dcb26e3a6c5cfe8e3b7553
DanielDimitrov5 <danieldimitrov2304@gmail.com>
2023-06-21 13:00:16 +0300

    Buy item, approve item, add existing collections

---
+++ src/pages/AddExistingCollection.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import { isAddress } from "ethers/lib/utils.js";
+import { addExistingCollection } from "../services/helpers";
+
+const AddExistingCollection = () => {
+    const [collectionAddress, setCollectionAddress] = useState("");
+    const [isValidAddress, setIsValidAddress] = useState(true);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const handleAddressChange = (e) => {
+        const address = e.target.value;
+        setCollectionAddress(address);
+        setIsValidAddress(isAddress(address));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (isAddress(collectionAddress)) {
+            setIsSubmitting(true);
+
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+
+            const result = await addExistingCollection(signer, collectionAddress);
+
+            if (result === 1) {
+                alert("Successfully added collection");
+            }
+
+            setIsSubmitting(false);
+        }
+
+        setCollectionAddress("");
+        setIsSubmitting(false);
+    };
+
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <br />
+                        <h1 className="text-center">Add Existing Collection</h1>
+                        <div className="text-center">
+                            <p>Enter the address of the collection you want to add</p>
+                        </div>
+                        {!isSubmitting ? (
+                            <form onSubmit={handleSubmit}>
+                                <div className="mb-3">
+                                    <label htmlFor="collectionName" className="form-label">
+                                        NFT Collection address
+                                    </label>
+                                    <input
+                                        onChange={handleAddressChange}
+                                        type="text"
+                                        className={`form-control ${isValidAddress ? "" : "is-invalid"}`}
+                                        id="collectionName"
+                                    />
+                                    {!isValidAddress && (
+                                        <div className="invalid-feedback">Invalid address</div>
+                                    )}
+                                </div>
+                                <div className="text-center">
+                                    {isValidAddress ? (
+                                        <button type="submit" className="btn btn-primary">
+                                            Add
+                                        </button>
+                                    ) : (
+                                        <button type="submit" className="btn btn-primary" disabled>
+                                            Add
+                                        </button>
+                                    )}
+                                </div>
+                            </form>
+                        ) : (
+                            <div className="text-center">
+                                <p>Adding collection...</p>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default AddExistingCollection;
---
+++ src/pages/ListItem.jsx
@@ -0,0 +1,116 @@
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { useAccount } from "wagmi";
+import { loadItemsForListing, listItemForSale } from "../services/helpers";
+import { Button, Popover, Cascader, InputNumber, Select, Space, Form } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEthereum } from "@fortawesome/free-brands-svg-icons"
+
+const ListItem = () => {
+
+    const [items, setItems] = useState([]);
+    const { isConnected, address } = useAccount();
+    const [isLoading, setIsLoading] = useState(true);
+    const [inputValue, setInputValue] = useState(0);
+    const [itemProperties, setItemProperties] = useState({});
+
+    const getData = async () => {
+        setIsLoading(true);
+        const provider = new ethers.providers.InfuraProvider(process.env.REACT_APP_NETWORK, process.env.REACT_APP_API_KEY);
+
+        const { filteredItems, nfts } = await loadItemsForListing(provider, address);
+
+        setItems({ filteredItems, nfts });
+        setIsLoading(false);
+    }
+
+    const setProperties = (nft, tokenId) => {
+        setItemProperties({ nft, tokenId });
+    }
+
+    const list = async () => {
+        setIsLoading(true);
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+
+        try {
+            const result = await listItemForSale(signer, itemProperties.nft, itemProperties.tokenId, ethers.utils.parseEther(inputValue.toString()));
+        } catch (error) {
+            console.log(error);
+
+        }
+        finally {
+            await getData();
+            setIsLoading(false);
+        }
+    }
+
+    const content = (
+        <div style={{ display: "flex", alignItems: "center" }}>
+            <InputNumber
+                step={0.1}
+                addonBefore="+"
+                addonAfter="ETH"
+                defaultValue={0}
+                min={0}
+                onChange={setInputValue}
+            />
+            {inputValue > 0 ? (
+                <Button type="primary" size="middle" shape="round" onClick={() => list()}>List</Button>
+            ) : (
+                <Button type="primary" size="middle" shape="round" disabled>List</Button>
+            )}
+        </div>
+    );
+
+    useEffect(() => {
+        getData();
+    }, [address]);
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <br />
+                    <h1 className="text-center">List Item</h1>
+                    {isLoading ? (
+                        <div className="text-center">
+                            <FontAwesomeIcon icon={faEthereum} spin size="2xl" />
+                        </div>
+                    ) : (
+                        <>
+                            {items.length === 0 ? (
+                                <div className="text-center">
+                                    <p>No items found</p>
+                                </div>
+                            ) : (
+                                <div className="row">
+                                    {items.nfts.map((nft, i) => (
+                                        <div className="col-12 col-md-6 col-lg-4" key={i}>
+                                            <div className="card mb-3">
+                                                <img src={nft.image} className="card-img-top" alt={nft.name} />
+                                                <div className="card-body">
+                                                    <h5 className="card-title">{nft.name}</h5>
+                                                    <p className="card-text">{nft.description}</p>
+                                                    <p className="card-text"><small className="text-muted">{nft.nft}</small></p>
+                                                    <Popover placement="bottom" content={content} title="Set price">
+                                                        <button onClick={() => {
+                                                            setInputValue(0);
+                                                            setProperties(nft.nft, nft.tokenId);
+                                                        }} className="btn btn-primary">List</button>
+                                                    </Popover>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ListItem;
