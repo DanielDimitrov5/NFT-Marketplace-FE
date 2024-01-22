4d20eb85b1768297a589d06f6cb9ca77496c9281
MertJSX <122701396+MertJSX@users.noreply.github.com>
2024-01-22 22:38:01 +0200

    Added dark theme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,67 +26,68 @@ import Footer from './layout/Footer';
 
 import { marketplaceContract, infuraProvider } from '../services/helpers';
 import nftABI from '../contractData/abi/NFT.json';
-import nftBytecode from '../contractData/NftBytecode.json'
+import nftBytecode from '../contractData/NftBytecode.json';
 import ErrorBoundary from './ErrorBoundary';
 import NFTMarketplaceSDK from 'nft-mp-sdk';
 import SDKContext from '../sdkContext';
 
 function App() {
-    const [sdk, setSdk] = useState(null);
+  const [sdk, setSdk] = useState(null);
 
-    const { provider } = configureChains([sepolia], [publicProvider()]);
+  const { provider } = configureChains([sepolia], [publicProvider()]);
 
-    const client = createClient({
-        provider,
-        autoConnect: true,
-    });
+  const client = createClient({
+    provider,
+    autoConnect: true,
+  });
 
-    useEffect(() => {
-        const sdk = new NFTMarketplaceSDK(
-            infuraProvider,
-            marketplaceContract.address,
-            marketplaceContract.abi,
-            nftABI,
-            nftBytecode.bytecode,
-            process.env.REACT_APP_IPFS_PROVIDER);
-        setSdk(sdk);
-    }, []);
-
-    if (!sdk) return null;
-
-    return (
-        <BrowserRouter>
-            <WagmiConfig client={client}>
-                <SDKContext.Provider value={sdk}>
-                    <ErrorBoundary>
-                        <div className="wrapper">
-                            <Header />
-                            <div className="main">
-                                <Routes>
-                                    <Route path="/" element={<Home />} />
-                                    <Route path="/item/:id" element={<Item />} />
-                                    <Route path="/create-collection" element={<CreateCollection />} />
-                                    <Route path="/mint-from" element={<MintFrom />} />
-                                    <Route path="/mint-from/:id" element={<MintNFT />} />
-                                    <Route path="/collections" element={<Collections />} />
-                                    <Route path="/collections/:id" element={<Collection />} />
-                                    <Route path="/add-item-from" element={<AddItem />} />
-                                    <Route path="/add-item-from/:id" element={<ChooseItem />} />
-                                    <Route path="/list-item" element={<ListItem />} />
-                                    <Route path="/my-items" element={<MyItems />} />
-                                    <Route path="/my-items/:id" element={<ItemDashboard />} />
-                                    <Route path='/my-offers' element={<MyOfferes />} />
-                                    <Route path='/user/:id' element={<UserPage />} />
-                                    <Route path="*" element={<NotFound />} />
-                                </Routes>
-                            </div>
-                            <Footer />
-                        </div>
-                    </ErrorBoundary>
-                </SDKContext.Provider>
-            </WagmiConfig>
-        </BrowserRouter>
+  useEffect(() => {
+    const sdk = new NFTMarketplaceSDK(
+      infuraProvider,
+      marketplaceContract.address,
+      marketplaceContract.abi,
+      nftABI,
+      nftBytecode.bytecode,
+      process.env.REACT_APP_IPFS_PROVIDER,
     );
+    setSdk(sdk);
+  }, []);
+
+  if (!sdk) return null;
+
+  return (
+    <BrowserRouter>
+      <WagmiConfig client={client}>
+        <SDKContext.Provider value={sdk}>
+          <ErrorBoundary>
+            <div className="wrapper">
+              <Header />
+              <div className="main dark-background">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/item/:id" element={<Item />} />
+                  <Route path="/create-collection" element={<CreateCollection />} />
+                  <Route path="/mint-from" element={<MintFrom />} />
+                  <Route path="/mint-from/:id" element={<MintNFT />} />
+                  <Route path="/collections" element={<Collections />} />
+                  <Route path="/collections/:id" element={<Collection />} />
+                  <Route path="/add-item-from" element={<AddItem />} />
+                  <Route path="/add-item-from/:id" element={<ChooseItem />} />
+                  <Route path="/list-item" element={<ListItem />} />
+                  <Route path="/my-items" element={<MyItems />} />
+                  <Route path="/my-items/:id" element={<ItemDashboard />} />
+                  <Route path="/my-offers" element={<MyOfferes />} />
+                  <Route path="/user/:id" element={<UserPage />} />
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </div>
+              <Footer />
+            </div>
+          </ErrorBoundary>
+        </SDKContext.Provider>
+      </WagmiConfig>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -16,132 +16,151 @@ import { useSDK } from '../../hooks/useSDK';
 import md5 from 'md5';
 
 function Header() {
-    const sdk = useSDK();
+  const sdk = useSDK();
 
-    const [currentSelection, setCurrentSelection] = useState('Home')
-    const [isOwner, setIsOwner] = useState(false)
-    const [marketplaceBalance, setMarketplaceBalance] = useState(0)
+  const [currentSelection, setCurrentSelection] = useState('Home');
+  const [isOwner, setIsOwner] = useState(false);
+  const [marketplaceBalance, setMarketplaceBalance] = useState(0);
 
-    const connector = new MetaMaskConnector({
-        chains: [sepolia],
-    });
+  const connector = new MetaMaskConnector({
+    chains: [sepolia],
+  });
 
-    const { isConnected, address } = useAccount();
-    const { connect, isLoading } = useConnect({
-        connector,
-    });
+  const { isConnected, address } = useAccount();
+  const { connect, isLoading } = useConnect({
+    connector,
+  });
 
-    const { data } = useBalance({
-        address,
-    });
+  const { data } = useBalance({
+    address,
+  });
 
-    const { disconnect } = useDisconnect();
+  const { disconnect } = useDisconnect();
 
-    const handleConnectButtonClick = async () => {
-        connect();
+  const handleConnectButtonClick = async () => {
+    connect();
+  };
+
+  const handleDisconnectButtonClick = async () => {
+    disconnect();
+  };
+
+  const handleSelect = e => {
+    setCurrentSelection(e.target.text);
+  };
+
+  const handleWithdraw = async () => {
+    if (isConnected) {
+      const result = await sdk.withdrawMoney();
+      if (result === 1) {
+        successMessage('Withdraw successful!');
+      }
+    }
+  };
+
+  const checkOwner = async () => {
+    if (isConnected) {
+      const result = await sdk.isMarketplaceOwner(address);
+      setIsOwner(result);
+    }
+  };
+
+  const getBalance = async () => {
+    if (isConnected) {
+      const result = await sdk.getMarketplaceBalance();
+
+      setMarketplaceBalance(result);
+    }
+  };
+
+  useEffect(() => {
+    if (isConnected) {
+      const signer = new ethers.providers.Web3Provider(window.ethereum).getSigner();
+      sdk.connectSigner(signer);
+    } else {
+      sdk.connectSigner(infuraProvider);
     }
 
-    const handleDisconnectButtonClick = async () => {
-        disconnect();
-    };
+    checkOwner();
+    getBalance();
+  }, [isConnected, address]);
 
-    const handleSelect = (e) => {
-        setCurrentSelection(e.target.text)
-    }
-
-    const handleWithdraw = async () => {
-        if (isConnected) {
-            const result = await sdk.withdrawMoney();
-            if (result === 1) {
-                successMessage('Withdraw successful!');
-            }
-        }
-    }
-
-    const checkOwner = async () => {
-        if (isConnected) {
-            const result = await sdk.isMarketplaceOwner(address);
-            setIsOwner(result);
-        }
-    }
-
-    const getBalance = async () => {
-        if (isConnected) {
-            const result = await sdk.getMarketplaceBalance();
-
-            setMarketplaceBalance(result);
-        }
-    }
-
-    useEffect(() => {
-        if (isConnected) {
-            const signer = new ethers.providers.Web3Provider(window.ethereum).getSigner();
-            sdk.connectSigner(signer);
-        }
-        else {
-            sdk.connectSigner(infuraProvider);
-        }
-
-        checkOwner();
-        getBalance();
-    }, [isConnected, address])
-
-    return (
-        <div className="header-wrapper">
-            <div className="header">
-                <div className="container d-flex justify-content-between align-item-center">
-                    <Link to={"/"}>
-                        <img
-                            src="https://limeacademy.tech/wp-content/uploads/2021/08/limeacademy_logo.svg"
-                            alt=""
-                        />
-                    </Link>
-                    <Nav>
-                        <NavDropdown
-                            title={currentSelection}
-                        >
-                            <NavDropdown.Item as={Link} to={'/'} onClick={handleSelect}>Home</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to={'/create-collection'} onClick={handleSelect}>Create NFT collection</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to={'/mint-from'} onClick={handleSelect}>Mint</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to={'/collections'} onClick={handleSelect}>View NFT collections</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to={'/add-item-from'} onClick={handleSelect}>Add item</ NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to={'/list-item'} onClick={handleSelect}>List item</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to={'/my-items'} onClick={handleSelect}>My Items</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to={'/my-offers'} onClick={handleSelect}>My Offers</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                    <div className="d-flex">
-                        {isLoading ? (
-                            <span>Connecting...</span>
-                        ) : isConnected ? (
-                            <>
-                                <div className="d-flex align-items-center justify-content-end">
-                                    <img
-                                        className="img-profile me-3"
-                                        src={`https://www.gravatar.com/avatar/${md5(address)}/?d=identicon`}
-                                        alt=""
-                                    />
-                                    <span>{truncate(address, 6)}</span>
-                                    <span className="mx-3">|</span>
-                                    <p>
-                                        <span className="fw-bold">Balance: </span>
-                                        <span>{Number(data && data.formatted).toFixed(3)} ETH</span>
-                                    </p>
-                                    <span className="mx-3">|</span>
-                                    <NavDropdown title="Account" id="basic-nav-dropdown">
-                                        <NavDropdown.Item onClick={handleDisconnectButtonClick}>Disconnect</NavDropdown.Item>
-                                        {isOwner && <NavDropdown.Item onClick={handleWithdraw}>Withdraw money</NavDropdown.Item>}
-                                    </NavDropdown>
-                                </div>
-                            </>
-                        ) : (
-                            <button className="btn btn-primary" onClick={handleConnectButtonClick}>Connect Metamask</button>
-                        )}
-                    </div>
+  return (
+    <div className="header-wrapper">
+      <div className="header dark-background-1">
+        <div className="container d-flex justify-content-between align-item-center">
+          <Link to={'/'}>
+            <img
+              src="https://limeacademy.tech/wp-content/uploads/2021/08/limeacademy_logo.svg"
+              alt=""
+            />
+          </Link>
+          <Nav>
+            <NavDropdown title={currentSelection}>
+              <NavDropdown.Item as={Link} to={'/'} onClick={handleSelect}>
+                Home
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to={'/create-collection'} onClick={handleSelect}>
+                Create NFT collection
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to={'/mint-from'} onClick={handleSelect}>
+                Mint
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to={'/collections'} onClick={handleSelect}>
+                View NFT collections
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to={'/add-item-from'} onClick={handleSelect}>
+                Add item
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to={'/list-item'} onClick={handleSelect}>
+                List item
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to={'/my-items'} onClick={handleSelect}>
+                My Items
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to={'/my-offers'} onClick={handleSelect}>
+                My Offers
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <div className="d-flex">
+            {isLoading ? (
+              <span>Connecting...</span>
+            ) : isConnected ? (
+              <>
+                <div className="d-flex align-items-center justify-content-end">
+                  <img
+                    className="img-profile me-3"
+                    src={`https://www.gravatar.com/avatar/${md5(address)}/?d=identicon`}
+                    alt=""
+                  />
+                  <span>{truncate(address, 6)}</span>
+                  <span className="mx-3">|</span>
+                  <p>
+                    <span className="fw-bold">Balance: </span>
+                    <span>{Number(data && data.formatted).toFixed(3)} ETH</span>
+                  </p>
+                  <span className="mx-3">|</span>
+                  <NavDropdown title="Account" id="basic-nav-dropdown">
+                    <NavDropdown.Item onClick={handleDisconnectButtonClick}>
+                      Disconnect
+                    </NavDropdown.Item>
+                    {isOwner && (
+                      <NavDropdown.Item onClick={handleWithdraw}>Withdraw money</NavDropdown.Item>
+                    )}
+                  </NavDropdown>
                 </div>
-            </div>
+              </>
+            ) : (
+              <button className="btn btn-primary" onClick={handleConnectButtonClick}>
+                Connect Metamask
+              </button>
+            )}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Header;
